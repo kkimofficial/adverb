78e6282b1cf341215a1ff2ec6286825d18ee1b0d
kkimofficial <98666177+kkimofficial@users.noreply.github.com>
2022-08-27 22:12:33 +0300

    Create magrittp.py

---
+++ magrittp.py
@@ -0,0 +1,14 @@
+class Magrittp:
+
+    def __init__(self, f, *args, **kwargs):
+        self.f = Magrittp.f(f, *args, **kwargs) if callable(f) else f
+
+    def __rshift__(self, other):
+        return Magrittp(other.f(self.f))
+
+    def __str__(self):
+        return str(self.f)
+
+    @staticmethod
+    def f(*args):
+        return lambda x: args[0](*(tuple([x if i == Magrittp else i for i in args[1:]]) if (Magrittp in args) else (x,) + args[1:]))
