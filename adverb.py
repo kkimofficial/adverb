044699764878e9f0c7c0f6ad13c9a4707ddf212a
kkimofficial <98666177+kkimofficial@users.noreply.github.com>
2022-08-30 20:55:44 +0300

    Update adverb.py

--- adverb.py
+++ adverb.py
@@ -2,9 +2,18 @@ class Adverb:
 
     def __init__(self, f, *args, **kwargs):
         self.f = Adverb.enlist(f, *args, **kwargs) if callable(f) else f
+        self.unwrap = False
 
     def __rshift__(self, other):
+        if other.unwrap:
+            return other.f(self.f)
         return Adverb(other.f(self.f)) if other != Adverb else self.f
+
+    def __add__(self, other):
+        if other == Adverb:
+            self.unwrap = True
+            return self
+        return Adverb(lambda x: other.f(self.f(x)))
 
     def __str__(self):
         return str(self.f)
